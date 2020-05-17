e6ad9e19f55058c075fb0f8d26fd6484bf5806e4
ManavRaja <manavrv2005@gmail.com>
2020-05-17 16:02:41 -0500

    Took out init function & default min & max args.

--- main.py
+++ main.py
@@ -17,11 +17,8 @@ pwd_max_length = int(input_int("Password Maximum Length:  "))
 
 
 class GeneratePassword:
-    def __init__(self, pwd_min_length=15, pwd_max_length=30):
-        self.pwd_min_length = pwd_min_length
-        self.pwd_max_length = pwd_max_length
 
-    def generate_password(self, pwd_min_length=15, pwd_max_length=30):
+    def generate_password(self, pwd_min_length, pwd_max_length):
         alphabet = string.ascii_letters + string.digits + string.punctuation
         number_between_min_max = randint(pwd_min_length, pwd_max_length)
         password = "".join(secrets.choice(alphabet) for i in range(number_between_min_max))
