067edd6bf4f947370f0bcc40a2fa1d8b7575d24d
ManavRaja <manavrv2005@gmail.com>
2020-05-17 16:13:59 -0500

    Took function out of class & deleted class.

--- main.py
+++ main.py
@@ -16,14 +16,11 @@ pwd_min_length = int(input_int("Password Minimum Length:  "))
 pwd_max_length = int(input_int("Password Maximum Length:  "))
 
 
-class GeneratePassword:
-
-    def generate_password(self, pwd_min_length, pwd_max_length):
-        alphabet = string.ascii_letters + string.digits + string.punctuation
-        number_between_min_max = randint(pwd_min_length, pwd_max_length)
-        password = "".join(secrets.choice(alphabet) for i in range(number_between_min_max))
-        return(f"Your Strong Passwprd is: \n{password}")
+def generate_password(pwd_min_length, pwd_max_length):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    number_between_min_max = randint(pwd_min_length, pwd_max_length)
+    password = "".join(secrets.choice(alphabet) for i in range(number_between_min_max))
+    return(f"Your Strong Passwprd is: \n{password}")
 
 
-pwd = GeneratePassword()
-print(pwd.generate_password(pwd_min_length, pwd_max_length))
+print(generate_password(pwd_min_length, pwd_max_length))
