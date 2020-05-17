96985e69086afad3ca7373ddf2d4fda049054f39
ManavRaja <manavrv2005@gmail.com>
2020-05-17 14:50:22 -0500

    Added minimum & maximum password length inputs.

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import string
 import secrets
 import random
+from random import randint
 
 
 def input_int(question):
     num = input(question)
     while num.isnumeric() == False:
-        print("That's not an integer. You can only input an integer! Please try again.")
+        print("Thats's not an integer. You can only input an integer! Please try again.")
         num = input(question)
     return num
 
 
-num = input_int("What should the character length of the password be?  ")
+pwd_min_length = int(input_int("Password Minimum Length:  "))
+pwd_max_length = int(input_int("Password Maximum Length:  "))
 
 
 class GeneratePassword:
-    def __init__(self, pwd_length=15):
-        self.pwd_length = pwd_length
+    def __init__(self, pwd_min_length=15, pwd_max_length=30):
+        self.pwd_min_length = pwd_min_length
+        self.pwd_max_length = pwd_max_length
 
-    def generate_pwd(self, pwd_length=15):
+    def generate_password(self, pwd_min_length=15, pwd_max_length=30):
         alphabet = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(secrets.choice(alphabet) for i in range(int(pwd_length)))
-        return(f"Your Strong Password is: \n{password}")
+        number_between_min_max = randint(pwd_min_length, pwd_max_length)
+        password = "".join(secrets.choice(alphabet) for i in range(number_between_min_max))
+        return(f"Your Strong Passwprd is: \n{password}")
 
 
 pwd = GeneratePassword()
-print(pwd.generate_pwd(int(num)))
+print(pwd.generate_password(pwd_min_length, pwd_max_length))
