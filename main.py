822a6f86d8c90bd6127b3d41fc50c658260c53f9
ManavRaja <manavrv2005@gmail.com>
2020-05-18 11:34:22 -0500

    Added ascii art when program starts, made generator take in only exact character length, Yes?No for upper & lower case characters, numbers, & special chracters. Also added try except.

--- main.py
+++ main.py
@@ -2,28 +2,58 @@ import string
 import secrets
 import random
 from random import randint
+import time
+from art import text2art
 
 
 def input_int(question):
     num = input(question)
     while num.isnumeric() == False:
-        print("Thats's not an integer. You can only input an integer! Please try again.")
+        print("That's not an integer. You can only input an integer! Please try again.")
         num = input(question)
     return num
 
 
-pwd_min_length = int(input_int("Password Minimum Length:  "))
-pwd_max_length = int(input_int("Password Maximum Length:  "))
-pwd_spec_charac = input("Should the password contain special characters?(Yes or No)  ")
+print(text2art("Password  Generator"))
+time.sleep(1.5)
+print(text2art("Coded  With  Python..."))
+time.sleep(1.5)
+print(text2art("By  @ManavRaja"))
+
+time.sleep(1)
+
+print("Please input a whole number for the password's length & 'Yes' or 'No' for all the other paramaters.(Case Sensitive)")
+time.sleep(3)
+pwd_length = int(input_int("Password Character Length:  "))
+pwd_upper = input("Should the password contain Upper Case letters?(Yes or No)  ")
+pwd_lower = input("Should the password contain Lower Case letters?(Yes or No)  ")
+pwd_num = input("Should the password contain Numbers?(Yes or No)  ")
+pwd_spec_charac = input("Should the password contain Special Characters?(Yes or No)  ")
 
 
-def generate_password(pwd_min_length, pwd_max_length, pwd_spec_charac):
-    alphabet = string.ascii_letters + string.digits
-    if pwd_spec_charac == "Yes":
-        alphabet = alphabet + string.punctuation
-    number_between_min_max = randint(pwd_min_length, pwd_max_length)
-    password = "".join(secrets.choice(alphabet) for i in range(number_between_min_max))
-    return(f"Your Strong Passwprd is: \n{password}")
+try:
+    def generate_password(pwd_length, pwd_upper, pwd_lower, pwd_num, pwd_spec_charac):
+
+        characters = ""
+    
+        if pwd_upper == "Yes":
+            characters = characters + string.ascii_uppercase
+        if pwd_lower == "Yes":
+            characters = characters + string.ascii_lowercase
+        if pwd_num == "Yes":
+            characters = characters + string.digits
+        if pwd_spec_charac == "Yes":
+            characters = characters + string.punctuation
+    
+        password = "".join(secrets.choice(characters) for i in range(pwd_length))
+        return(f"Your Strong Password is: \n{password}")
 
 
-print(generate_password(pwd_min_length, pwd_max_length, pwd_spec_charac))
+    print(generate_password(pwd_length, pwd_upper, pwd_lower, pwd_num, pwd_spec_charac))
+except IndexError:
+    print("You can't set all paramaters to 'No' and have it generate you a password! Please try again and set atleast 1 of the paramaters to 'Yes'.")
+    while pwd_upper == pwd_lower == pwd_num == pwd_spec_charac == "No":
+        pwd_upper = input("Should the password contain Upper Case letters?(Yes or No)  ")
+        pwd_lower = input("Should the password contain Lower Case letters?(Yes or No)  ")
+        pwd_num = input("Should the password contain Numbers?(Yes or No)  ")
+        pwd_spec_charac = input("Should the password contain Special Characters?(Yes or No)  ")
